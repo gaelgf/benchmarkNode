a92a6c1a266d4c69c714b63fb704d9914132707b
gaelgf <gael.graffion@gmail.com>
2017-02-15 21:48:38 +0100

    Hello l'ESGI !

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Bonjour l\'ESGI !</h1>');
   res.end();
-}).listen(8080);
+}).listen(80);
